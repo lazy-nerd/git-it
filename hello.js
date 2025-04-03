21eb23755bdedb7030d300e0a7354109adfc4ac6
Viktor <derfikus@gmail.com>
2025-04-03 10:14:00 +0300

    I'm breaking stuff

--- hello.js
+++ hello.js
@@ -2,3 +2,6 @@ console.log('Hello, Git!');
 console.log('Hello, GitHub!');
 console.log('Just a normal code!');
 console.log('Just a normal code before a bad one!');
+
+# really BaseAudioContext
+console.log('A nasty code that brakes things');
